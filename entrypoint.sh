1f0d693bf35069e4cca63c0741797e4c15318c54
Ahmad Ansori Palembani <palembani@gmail.com>
2024-10-04 09:22:41 +0700

    feat: Enable user accounts

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,14 @@
 
 if [ ! -e "./persist/config/config.yaml" ]; then
     echo "Resource not found, copying from defaults: config.yaml"
-    sed 's/^whitelistMode: true/whitelistMode: false/g' ./default/config.yaml |\
-    sed 's/^basicAuthMode: false/basicAuthMode: true/g' > ./persist/config/config.yaml
+    cat ./default/config.yaml |\
+        sed 's/^listen: false/listen: true/g' |\
+        sed 's/^whitelistMode: true/whitelistMode: false/g' |\
+        sed 's/^basicAuthMode: false/basicAuthMode: true/g' |\
+        sed 's/^enableUserAccounts: false/enableUserAccounts: true/g' |\
+        sed 's/^enableDiscreetLogin: false/enableDiscreetLogin: true/g' |\
+        sed 's/^securityOverride: false/securityOverride: true/g' \
+            > ./persist/config/config.yaml
 fi
 
 # Start the server
