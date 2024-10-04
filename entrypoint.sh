388f7fbb46b114a9cb5cc3311fb1a7f507a81572
Ahmad Ansori Palembani <palembani@gmail.com>
2024-10-04 08:41:19 +0700

    fix: Handle Railway's volume quirk

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-ls -l persist
+# Because railway's volume is a bitch, it completely wiped ./persist after you attach a volume to it
+[ ! -d "./persist/config" ] && mv ./_config ./persist/config
+[ ! -d "./persist/data" ] && mv ./_data ./persist/data
+[ ! -d "./persist/plugins" ] && mv ./_plugins ./persist/plugins
 
-if [ ! -e "persist/config/config.yaml" ]; then
+if [ ! -e "./persist/config/config.yaml" ]; then
     echo "Resource not found, copying from defaults: config.yaml"
-    cp -r "default/config.yaml" "persist/config/config.yaml"
+    cp -r "./default/config.yaml" "./persist/config/config.yaml"
 fi
 
 # Start the server
