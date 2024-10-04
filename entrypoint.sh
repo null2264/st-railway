b4aeeb04df3ec9d737c3bd491ab21b43f8e643d4
Ahmad Ansori Palembani <palembani@gmail.com>
2024-10-04 08:56:05 +0700

    fix: Disable whitelist

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@
 
 if [ ! -e "./persist/config/config.yaml" ]; then
     echo "Resource not found, copying from defaults: config.yaml"
-    cp -r "./default/config.yaml" "./persist/config/config.yaml"
+    sed 's/^whitelistMode: true/whitelistMode: false/g' ./default/config.yaml > ./persist/config/config.yaml
 fi
 
 # Start the server
