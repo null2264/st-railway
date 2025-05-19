7570fbf692e70e6e6fa2d75033e0889024dfc2ab
Ahmad Ansori Palembani <palembani@gmail.com>
2025-05-19 08:19:17 +0700

    refactor: Simplify Dockerfile and the entrypoint

---
+++ wrapper.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+SILLYTAVERN_DISCREET_LOGIN=${SILLYTAVERN_DISCREET_LOGIN:-false}
+
+envsubst < ./config.template.yaml > ./persist/config/config.yaml
+# if [ ! -e "./persist/config/config.yaml" ]; then
+#     echo "Config not found, copying from template"
+#     envsubst < ./config.template.yaml > ./persist/config/config.yaml
+# fi
+
+tini -- ./docker-entrypoint.sh
