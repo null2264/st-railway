d2a7b5a60e7180b795eddd70841aba1998652b92
Ahmad Ansori Palembani <palembani@gmail.com>
2025-05-19 08:39:25 +0700

    fix: Use 'export'

--- wrapper.sh
+++ wrapper.sh
@@ -4,7 +4,7 @@
 [ ! -d "./persist/data" ] && mkdir ./persist/data
 [ ! -d "./persist/plugins" ] && mkdir ./persist/plugins
 
-SILLYTAVERN_DISCREET_LOGIN=${SILLYTAVERN_DISCREET_LOGIN:-false}
+export SILLYTAVERN_DISCREET_LOGIN=${SILLYTAVERN_DISCREET_LOGIN:-false}
 
 envsubst < ./config.template.yaml > ./persist/config/config.yaml
 # if [ ! -e "./persist/config/config.yaml" ]; then
