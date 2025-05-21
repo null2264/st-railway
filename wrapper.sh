6268ba2e2e3e5c34e6ba4d90e68b48ade240aaeb
Ahmad Ansori Palembani <palembani@gmail.com>
2025-05-21 09:29:13 +0700

    feat: IPv6 support
    Just in case...

--- wrapper.sh
+++ wrapper.sh
@@ -8,6 +8,8 @@
 export PORT=${PORT:-8080}
 export SILLYTAVERN_DISCREET_LOGIN=${SILLYTAVERN_DISCREET_LOGIN:-false}
 export SILLYTAVERN_LOG_LEVEL=${SILLYTAVERN_LOG_LEVEL:-3}
+export SILLYTAVERN_ENABLE_IPV6=${SILLYTAVERN_ENABLE_IPV6:-false}
+export SILLYTAVERN_PREFER_IPV6=${SILLYTAVERN_PREFER_IPV6:-false}
 
 envsubst < ./config.template.yaml > ./persist/config/config.yaml
 # if [ ! -e "./persist/config/config.yaml" ]; then
