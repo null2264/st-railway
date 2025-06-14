f1e84706fffd86dd88eb9a1ffea1debe84ba7bc9
Ahmad Ansori Palembani <palembani@gmail.com>
2025-06-14 11:52:33 +0700

    chore: Update to v1.13.0

--- wrapper.sh
+++ wrapper.sh
@@ -5,11 +5,14 @@
 [ ! -d "./persist/data" ] && mv ./_data ./persist/data
 [ ! -d "./persist/plugins" ] && mv ./_plugins ./persist/plugins
 
+#region Environment variables' default values
 export PORT=${PORT:-8080}
 export SILLYTAVERN_DISCREET_LOGIN=${SILLYTAVERN_DISCREET_LOGIN:-false}
 export SILLYTAVERN_LOG_LEVEL=${SILLYTAVERN_LOG_LEVEL:-3}
 export SILLYTAVERN_ENABLE_IPV6=${SILLYTAVERN_ENABLE_IPV6:-false}
 export SILLYTAVERN_PREFER_IPV6=${SILLYTAVERN_PREFER_IPV6:-false}
+export SILLYTAVERN_GEMINI_API_VERSION="${SILLYTAVERN_GEMINI_API_VERSION:-v1beta}"
+#endregion
 
 envsubst < ./config.template.yaml > ./persist/config/config.yaml
 # if [ ! -e "./persist/config/config.yaml" ]; then
