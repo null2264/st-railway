a64de628c3fb6af91023ed29317c077f786c3b48
Ahmad Ansori Palembani <palembani@gmail.com>
2025-09-18 10:45:30 +0700

    feat: Automate version fetch

---
+++ main.py
@@ -0,0 +1,20 @@
+import os
+import requests
+import semver
+
+
+def main():
+    token = os.environ.get("GITHUB_TOKEN")
+    headers: dict[str, str] = {}
+    if token:
+        headers["Authorization"] = token
+
+    r = requests.get("https://api.github.com/repos/SillyTavern/SillyTavern/releases", headers=headers).json()[:5]
+    versions = [{ "version": i["tag_name"] } for i in r if i.get("tag_name")]
+    versions.insert(0, { "version": "latest" })
+    rt = { "includes": versions }
+    print(rt)
+
+
+if __name__ == "__main__":
+    main()
